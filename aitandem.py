a3db388fe38683998e7ed573f870a3d6dae2056b
Ali Rostami <rostamiev@gmail.com>
2021-01-18 15:32:03 +0100

    initial commit

---
+++ aitandem.py
@@ -0,0 +1,82 @@
+from vosk import Model, KaldiRecognizer
+from flask import Flask, request, send_from_directory, render_template
+import os, wave, json, base64, datetime, csv
+
+app = Flask(__name__)
+
+
+def RecognizeVoice(audio_file, language):
+    if not os.path.exists("/var/www/flask/data/model_" + language):
+        print(
+            "Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+        exit(1)
+    wf = wave.open(audio_file, "rb")
+    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+        print("Audio file must be WAV format mono PCM.")
+        exit(1)
+    model = Model("/var/www/flask/data/model_" + language)
+    rec = KaldiRecognizer(model, wf.getframerate())
+    all_res = ""
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            #            print(rec.Result())
+            all_res = all_res + " " + json.loads(rec.Result())['text']
+    #            print(all_res)
+    #        else:
+    #            print(rec.PartialResult())
+    res = rec.FinalResult()
+    t = json.loads(res)
+    all_res = all_res + " " + t['text']
+    return all_res
+
+
+@app.route("/")
+def hello():
+    return render_template("VoiceService.html")
+
+@app.route("/record", methods=['POST'])
+def record():
+    language = request.headers.get('lang')
+    tag = request.headers.get('tag')
+    file_name = '/var/www/flask/data/' + str(datetime.datetime.now()).replace(' ', '-') + language + tag + ".wav"
+    with open(file_name, 'wb') as f:
+        f.write(request.data)
+    res = RecognizeVoice(file_name, language)
+    return res
+
+@app.route("/question", methods=['POST'])
+def question():
+    import fasttext as ft
+
+    model_title = ft.load_model('data/title.bin')
+    model_subtitle = ft.load_model('data/subtitle.bin')
+
+    lang = request.form['lang']
+    text = request.form['text']
+    title_predict = model_title.predict(text, k=3)
+    subtitle_predict = model_subtitle.predict(text, k=3)
+
+    res = ""
+    res2 = ""
+    res3 = ""
+    res4 = ""
+    for i in range(0, 3):
+        res += map_label_title[title_predict[0][i].split(',')[0]] + ","
+        res2 += str(title_predict[1][i])[0:14] + ","
+        res3 += map_label_subtitle[subtitle_predict[0][i].split(',')[0]] + ","
+        res4 += str(subtitle_predict[1][i])[0:14] + ","
+
+    res = res[0:-1]
+    res2 = res2[0:-1]
+    res3 = res3[0:-1]
+    res4 = res4[0:-1]
+
+    res = res + "---" + res2 + ":" + res3 + "---" + res4
+    return res
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/app.js
@@ -0,0 +1,68 @@
+URL = window.URL || window.webkitURL;
+var gumStream;
+var rec;
+var input;
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext;
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+$('#loader').hide();
+function startRecording() {
+    var constraints = { audio: true, video:false }
+	recordButton.disabled = true;
+	stopButton.disabled = false;
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+		audioContext = new AudioContext();
+		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+		gumStream = stream;
+		input = audioContext.createMediaStreamSource(stream);
+		rec = new Recorder(input,{numChannels:1})
+		rec.record()
+	}).catch(function(err) {
+    	  recordButton.disabled = false;
+    	  stopButton.disabled = true;
+	});
+}
+
+function stopRecording() {
+    stopButton.disabled = true;
+    recordButton.disabled = false;
+    rec.stop();
+    gumStream.getAudioTracks()[0].stop();
+    rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+	var filename = new Date().toISOString();
+	au.controls = true;
+	au.src = url;
+	link.href = url;
+	//link.download = filename+".wav";
+	link.innerHTML = "Save to disk";
+	li.appendChild(au);
+	li.appendChild(document.createTextNode(filename+".wav "))
+	li.appendChild(link);
+	$('#loader').show();
+	var xhr=new XMLHttpRequest();
+	xhr.onload=function(e) {
+		if(this.readyState === 4) {
+		     li.appendChild(document.createElement("BR"));
+		     tmp = document.createElement("H3");
+		     tmp.appendChild(document.createTextNode(e.target.responseText));
+		     li.appendChild(tmp);
+		     li.appendChild(document.createElement("BR"));
+		     $('#loader').hide()
+	     }
+        };
+	xhr.open("POST","record",true);
+	xhr.setRequestHeader('lang', $('#SelectLang').val());
+	xhr.setRequestHeader('tag',  $('#tag').val());
+	xhr.send(blob);
+	li.appendChild(document.createTextNode (" "));
+	recordingsList.appendChild(li);
+}
